1df2e6778b8f29c0856f823c22567d97ae3279c4
Opuchlich, Paul <paul.opuchlich@sap.com>
2018-03-29 11:12:04 +0200

    Sense Model Creation finished for one parser

--- Coding/read_write_files.py
+++ Coding/read_write_files.py
@@ -4,7 +4,9 @@ import json
 def read_json(file_path):
     return [json.loads(x) for x in open(file_path)]
 
-def save_json(alignments,file_path):
+def save_json(data,file_path):
     with open(file_path, 'w') as f:
-        for alignment in alignments:
-            f.write(json.dumps(alignment)+"\n")
+        for date in data:
+            f.write(json.dumps(date)+"\n")
+
+def save_model_json
