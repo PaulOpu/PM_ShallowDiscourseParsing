9fc01a0ddae0bc7953f6da04f3175e821221b067
Opuchlich, Paul <paul.opuchlich@sap.com>
2018-03-30 22:23:48 +0200

    Evaluation Max Agreement

--- Coding/helper_functions.py
+++ Coding/helper_functions.py
@@ -5,6 +5,12 @@ sys.path.append("conll16st")
 import aligner as aligner
 import partial_scorer as ps
 import read_write_files as rw
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 def change_sets_to_list(alignments):
     for gold,pred in alignments:
@@ -60,3 +66,44 @@ def get_sense_lists(relations):
     parser_senses_zip = list(zip(*parser_senses))
     
     return gold_senses,parser_senses_zip,parser_names
+
+
+def plot_confusion_matrix(cm,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    classes = []
+    alpha = cm.alphabet
+    for i in xrange(alpha.size()):
+        classes += [alpha.get_label(i)]
+
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
